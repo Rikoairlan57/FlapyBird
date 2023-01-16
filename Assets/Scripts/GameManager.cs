163597b6a0f1350926d90192e4528bcf54176ed4
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2023-01-16 18:07:55 +0700

    display score, start button trigger, game over, pipes trigger game over

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,25 +1,62 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public Player player;
+    private Player player;
+    private Spawner spawner;
 
     public Text scoreText;
+    public GameObject playButton;
+    public GameObject gameOver;
+    public int score { get; private set; }
 
-    public gameObject playButton;
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
 
-    public gameObject gameOver;
+        player = FindObjectOfType<Player>();
+        spawner = FindObjectOfType<Spawner>();
 
-    private int score;
+        Pause();
+    }
+
+    public void Play()
+    {
+        score = 0;
+        scoreText.text = score.ToString();
+
+        playButton.SetActive(false);
+        gameOver.SetActive(false);
+
+        Time.timeScale = 1f;
+        player.enabled = true;
+
+        Pipes[] pipes = FindObjectsOfType<Pipes>();
+
+        for (int i = 0; i < pipes.Length; i++) {
+            Destroy(pipes[i].gameObject);
+        }
+    }
 
     public void GameOver()
     {
-        Debug.Log("Game Over");
+        playButton.SetActive(true);
+        gameOver.SetActive(true);
+
+        Pause();
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        player.enabled = false;
     }
 
     public void IncreaseScore()
     {
         score++;
+        scoreText.text = score.ToString();
     }
+
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,63 +2,70 @@
 
 public class Player : MonoBehaviour
 {
-  
-  private SpriteRenderer spriteRenderer;
+    private SpriteRenderer spriteRenderer;
+    public Sprite[] sprites;
+    private int spriteIndex;
 
-  public Sprite[] sprites;
+    public float strength = 5f;
+    public float gravity = -9.81f;
+    public float tilt = 5f;
 
-  private int spriteIndex;
+    private Vector3 direction;
 
-  private Vector3 direction;
-
-  public float gravity = -9.8f;
-
-  public float strength = 5f;
-
-    private void Awake(){
+    private void Awake()
+    {
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    private void Start() {
-        InvokeRepeating(nameof(AnimateSprite), 0.15f, 0.15f );   
-    }
-
-  private void Update()
-  {
-    if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)){
-        direction = Vector3.up * strength;
-    }
-
-    if (Input.touchCount > 0)
+    private void Start()
     {
-        Touch touch = Input.GetTouch(0);
+        InvokeRepeating(nameof(AnimateSprite), 0.15f, 0.15f);
+    }
 
-        if (touch.phase == TouchPhase.Began) {
+    private void OnEnable()
+    {
+        Vector3 position = transform.position;
+        position.y = 0f;
+        transform.position = position;
+        direction = Vector3.zero;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) {
             direction = Vector3.up * strength;
+        }
+
+      
+        direction.y += gravity * Time.deltaTime;
+        transform.position += direction * Time.deltaTime;
+
+      
+        Vector3 rotation = transform.eulerAngles;
+        rotation.z = direction.y * tilt;
+        transform.eulerAngles = rotation;
+    }
+
+    private void AnimateSprite()
+    {
+        spriteIndex++;
+
+        if (spriteIndex >= sprites.Length) {
+            spriteIndex = 0;
+        }
+
+        if (spriteIndex < sprites.Length && spriteIndex >= 0) {
+            spriteRenderer.sprite = sprites[spriteIndex];
         }
     }
 
-    direction.y += gravity * Time.deltaTime;
-    transform.position += direction * Time.deltaTime;
-  }
-
-  private void AnimateSprite()
-  {
-    spriteIndex++;
-
-    if(spriteIndex >= sprites.Length) {
-        spriteIndex = 0;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Obstacle")) {
+            FindObjectOfType<GameManager>().GameOver();
+        } else if (other.gameObject.CompareTag("Scoring")) {
+            FindObjectOfType<GameManager>().IncreaseScore();
+        }
     }
 
-    spriteRenderer.sprite = sprites[spriteIndex];
-  }
-
-  private void OnTriggerEnter2D(Collider2D other)
-  {
-      if (other.gameObject.tag == "Obstacle") {
-        FindObjectOfType<GameManager>().GameOver();
-      } else if (other.gameObject.CompareTag("Scoring")) {
-        FindObjectOfType<GameManager>().IncreaseScore();
-      }
-  }
 }

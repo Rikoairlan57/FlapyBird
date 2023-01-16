c5d5d6b2adf862c4cfee8ab2f2df7408c88f3ec5
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2023-01-16 18:16:16 +0700

    undo

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Player player;
+
+    public Text scoreText;
+
+    public gameObject playButton;
+
+    public gameObject gameOver;
+
+    private int score;
+
+    public void GameOver()
+    {
+        Debug.Log("Game Over");
+    }
+
+    public void IncreaseScore()
+    {
+        score++;
+    }
+}
